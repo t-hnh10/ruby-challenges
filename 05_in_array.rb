1f553280c421076c74428b74157f4c1ba15fadec
t-hnh10 <t.hnh@tuta.io>
2019-04-08 11:04:27 +1000

    commented beast mode in 05_in_array

--- 05_in_array.rb
+++ 05_in_array.rb
@@ -27,3 +27,36 @@ puts in_array("hello", ["hi","howdy","hello"])
 # Beast Mode:
 
 # Redo the challenge but try solving it again in a different way.
+
+=begin def in_array (needle, haystack)
+  # first approach
+  return haystack.include?(needle)
+
+  #second approach
+  haystack.each { |h| return true if h == needle }
+  return false
+
+  haystack.each do |h|
+    return true if h == needle
+    end
+    return false
+
+    # setting up variable i
+    i = 0
+    # we want to go through this loop once through our element
+    while i < haystack.length do
+      if haystack[i] == needle
+        return true # return i if question asked to return index of needle
+      end
+      i += 1
+    end
+      return false
+  end
+# two dots in a range say 'include the last value', three dots say 'don't include the last value
+  for i in (0..haystack.length - 1)
+    if haystack[i] == needle
+    return true
+    end
+  end
+  return false
+=end
