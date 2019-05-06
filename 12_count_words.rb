bf03f110d8954f0e673bd7110ae6b156fee09c71
t-hnh10 <t.hnh@tuta.io>
2019-05-06 22:45:49 +1000

    fixed conflicts

--- 12_count_words.rb
+++ 12_count_words.rb
@@ -10,18 +10,36 @@
 
 # Test your solution by running 12_count_words
 
+# Ask user for input
+puts "Please give me a phrase of list of words"
+input = gets.strip
 
-puts "Please enter some words, any words!"
-input = gets.chomp
-
-def count_words(words)
-    word_hash = Hash.new(0)
-    words.downcase.split.each do |word|
-        word_hash[word] += 1
+# Count occurrence of each word in string
+# Input is a string (phrase)
+# Prints list of words with occurrence of each word (case insensitive)
+def count_words(phrase)
+    words_hash = Hash.new(0) # If I add a new key, the initial value is 0
+    phrase.split("").each do |word|
+        words_hash[word.downcase] += 1
     end
-    return word_hash
+    print_word_count(words_hash)
+    return words_hash
 end
 
+def print_word_count(words_hash)
+    words_hash.each do |key, value|
+        puts "#{key} #{value}"
+    end
+end
+
+# # Initial attempt
+# def count_words(words)
+#     word_hash = Hash.new(0)
+#     words.downcase.split.each do |word|
+#         word_hash[word] += 1
+#     end
+#     return word_hash
+# end
 
 # Beast Mode:
 # Change your code so that it doesn't matter if capital letters are used,
