da5daedda2b7dbe3d830482190a57da60c34d2f3
Tony Huynh <t.hnh@tuta.io>
2019-06-18 09:31:17 +1000

    added 32

--- 31_missing_letter.rb
+++ 31_missing_letter.rb
@@ -1,10 +1,10 @@
 # Missing letter
 # Difficulty: medium/hard
 
-# Write a method that takes an array of consecutive (increasing) 
+# Write a method that takes an array of consecutive (increasing)
 # letters as input and that returns the missing letter in the array.
 
-# You will always get an valid array. And it will be always exactly 
+# You will always get an valid array. And it will be always exactly
 # one letter be missing. The length of the array will always be at least 2.
 # The array will always contain letters in only one case.
 
